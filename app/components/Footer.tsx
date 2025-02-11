186787c0540417c14a04b3284bdb94a34e3e45e9
ABDUL RAHIM <abdulrahim74264@gmail.com>
2025-02-11 22:18:33 +0530

    Link Updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,15 +7,15 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-600 dark:text-gray-400 mb-4 md:mb-0">&copy; 2025 Abdul Rahim. All rights reserved.</p>
           <div className="flex space-x-4">
-            <a href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
+            <a href="https://github.com/Abdul0925" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
               <Github className="w-6 h-6" />
             </a>
-            <a href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
+            <a href="https://linkedin.com/in/Abdul0925/" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
               <Linkedin className="w-6 h-6" />
             </a>
-            <a href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
+            {/* <a href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white">
               <Twitter className="w-6 h-6" />
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
