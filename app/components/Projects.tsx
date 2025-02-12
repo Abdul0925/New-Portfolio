0a74c492ef6861e39c3da8642812608ea4d85127
ABDUL RAHIM <abdulrahim74264@gmail.com>
2025-02-12 21:42:19 +0530

    Project Link Added

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -7,17 +7,26 @@ const projects = [
     title: "Hackathon Website",
     description: "An event management website that enables participants and organizers to seamlessly plan, coordinate, and manage hackathons with ease.",
     image: "/hackathon-website.png",
+    link: "https://rthack.raisoni.net",
   },
   {
     title: "Placement Hub",
     description: "A comprehensive portal designed for college students, administrators and companies to simplify and streamline the recruitment process efficiently.",
     image: "/placement-hub.jpg",
+    link: "https://github.com/Abdul0925",
   },
   {
-    title: "Task Tracker CLI",
-    description: "Task tracker is a project used to track and manage your tasks. In this task, you will build a simple command line interface (CLI) to track what you need to do, what you have done, and what you are currently working on.",
-    image: "/task-cli.png",
+    title: "Online Buzzer System",
+    description: "Online Buzzer is a system built for quiz competitions organized by Encarta IT Cell.",
+    image: "/buzzer.png",
+    link: "https://github.com/Abdul0925/Encarta-Buzzer",
   },
+  // {
+  //   title: "Task Tracker CLI",
+  //   description: "Task tracker is a project used to track and manage your tasks. In this task, you will build a simple command line interface (CLI) to track what you need to do, what you have done, and what you are currently working on.",
+  //   image: "/task-cli.png",
+  //   link: "https://github.com/Abdul0925",
+  // },
 ]
 
 export default function Projects() {
@@ -39,7 +48,7 @@ export default function Projects() {
               <CardContent className="p-6">
                 <h3 className="text-xl font-semibold mb-2 text-gray-800 dark:text-white">{project.title}</h3>
                 <p className="text-gray-600 dark:text-gray-300 mb-4">{project.description}</p>
-                <Button variant="outline">View Project</Button>
+                <Button variant="outline"><a href={project.link} target="_blank">View Project</a></Button>
               </CardContent>
             </Card>
           ))}
